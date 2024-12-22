b17674c84d099347584b90067b211b1fc54f357a
Yasmin Ferrari Dias <158753555+YasminFerrariDias@users.noreply.github.com>
2024-12-22 13:05:45 -0300

    Add files via upload

---
+++ PrecoDeCustoEVenda.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class PrecoDeCustoEVenda {
+  public static void main(String[] args) {
+    Scanner ler = new Scanner(System.in);
+    float custo, perc, total;
+    String produto;
+    System.out.print("Informe o produto: ");
+    produto = ler.nextLine();
+    System.out.print("Informe o preço de custo: ");
+    custo = ler.nextFloat();
+    System.out.print("Informe o percentual de aumento: ");
+    perc = ler.nextFloat();
+    total = custo+(custo*perc)/100;
+    System.out.print("O valor de " + produto + " é de " + total);
+  }
+}
